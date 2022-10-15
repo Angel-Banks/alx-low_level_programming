72b2443bdeae3c17e24283536b21a32d0131a359
Angel-Banks <angelonuchuks@gmail.com>
2022-10-15 06:46:20 -0700

    Update Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ *main - Prints the alphabetic
+ *
+ *Return: Always (Success)
+ *
+ */
+
+
+
+int main(void)
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
