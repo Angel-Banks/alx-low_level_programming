2b50d5e9131d0687e48081b286916c3a4398fd72
Angel-Banks <angelonuchuks@gmail.com>
2022-11-13 12:19:54 -0800

    Update Files

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,8 +2,6 @@
 
 #include "main.h"
 
-
-
 /**
 * *_realloc - reallocates a memory block
 * @ptr: pointer to the peviously allocated memory
@@ -13,53 +11,30 @@
 */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-
 {
-
 	char *new, *old;
-
 	unsigned int i;
 
 	if (new_size == old_size)
-
 		return (ptr);
-
 	if (ptr && new_size == 0)
 
 	{
-
 		free(ptr);
-
 		return (NULL);
-
 	}
-
 	if (!ptr)
-
 		return (malloc(new_size));
-
 	old = ptr;
-
 	new = malloc(new_size);
-
 	if (!new)
-
 		return (NULL);
-
 	if (new_size < old_size)
-
 		for (i = 0; i < new_size; i++)
-
 			new[i] = old[i];
-
 	else
-
 		for (i = 0; i < old_size; i++)
-
 			new[i] = old[i];
-
 	free(ptr);
-
 	return (new);
-
 }
