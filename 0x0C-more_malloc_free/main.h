12bc12bb4bf2612a089a060a6c35b25b0fdbaa95
Angel-Banks <angelonuchuks@gmail.com>
2022-11-13 11:42:31 -0800

    Update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,12 +1,10 @@
-lines (9 sloc)  321 Bytes
+#ifndef MAIN_H
 
 
 
-#include <stdio.h>
+#define MAIN_H
 
-#include <stdlib.h>
 
-#include <limits.h>
 
 int _putchar(char c);
 
@@ -19,5 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
 
 #endif
